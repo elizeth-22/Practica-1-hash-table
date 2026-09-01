ac76730b969d6b42fec012b134cc4b160fece8db
gsutov <gsuxie.grimstar@ciencias.unam.mx>
2026-09-01 12:53:48 -0600

    feat: create hash table structure

---
+++ TablaHash.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class TablaHash{
+    ArrayList<Nodo>[] = tabla[];
+    tabla = new ArrayList[7];
+
+    for (int i = 0; i < tabla.length; i++){
+        tabla[i] = new ArrayList<Nodo>();
+    }
+}
