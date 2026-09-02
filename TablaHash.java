fe70ccd41baa8e32d4963a9934c1ee500b2b48a6
Lizeth Suemy Esquivel Izquierdo <elizethsuemy22@ciencias.unam.mx>
2026-09-01 21:35:55 -0600

    implementacion de imprimir tabla

--- TablaHash.java
+++ TablaHash.java
@@ -1,25 +1,28 @@
 import java.util.ArrayList;
 
 public class TablaHash{
-    ArrayList<Nodo>[] = tabla[];
-    tabla = new ArrayList[7];
+    ArrayList<Nodo>[] tabla;
 
-    for (int i = 0; i < tabla.length; i++){
-        tabla[i] = new ArrayList<Nodo>();
+    public void tabla(){
+        tabla = new ArrayList[7];
+        for (int i = 0; i < tabla.length; i++){
+            tabla[i] = new ArrayList<Nodo>();
+        } 
     }
+    
 
     private int hash(int key){
         return key % tabla.length;
     }
 
     public void insertar (int key, String valor){
-        int indice = obtenerIndice(key);
+        int indice = hash(key);
         ArrayList<Nodo> cubeta = tabla[indice];
     } 
 
     public int buscar(int key){
-        
-        int indice = obtenerIndice(key);
+            
+        int indice = hash(key);
         ArrayList<Nodo> cubeta = tabla[indice];
 
         for (Nodo nodo : cubeta ) {
@@ -30,4 +33,29 @@ public class TablaHash{
             }
         }
     }
+
+    /**
+     * Metodo publico para generar la impresión visual de la tabla
+     * @return impresion de la tabla
+     * */
+    @Override
+    public String toString(){
+        String acumula = ""; 
+        //recorre la tabla
+        for (int i = 0; i< tabla.length  ; i++) {
+            acumula += i + " -> ";
+            //recorre cada cubeta
+            for (int j = 0; j < tabla[i].size(); j++ ) {
+                Nodo nodo = tabla[i].get(j);
+                if (j < tabla[i].size() - 1) { 
+                    acumula += "(" + nodo.key + "," + nodo.valor + ")" + " -> ";  
+                } else{
+                    acumula += "(" + nodo.key + "," + nodo.valor + ")";
+                }
+            }
+            acumula += "\n";
+        }
+        return acumula;
+    }
+
 }
